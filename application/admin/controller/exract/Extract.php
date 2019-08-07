9e4a82cdd1a446c7d49d8e98b06798b8cf259cef
hu0626 <hu920629@126.com>
2019-08-07 13:49:25 +0800

    提现管理

--- application/admin/controller/exract/Extract.php
+++ application/admin/controller/exract/Extract.php
@@ -18,6 +18,7 @@ use app\wap\model\user\UserBill;
 use service\UtilService as Util;
 use think\db;
 use think\Request;
+use service\JsonService;
 
 
 /**
@@ -46,7 +47,7 @@ class Extract extends AuthController
             'where'=>$where,
         ]);
 
-        $count= DB::name('user_extract')->where($where)->field('*')->count();
+        $count= DB::name('extract')->where($where)->field('*')->count();
 
         //获取每页显示的条数
         $limit= Request::instance()->param('limit');
@@ -55,7 +56,7 @@ class Extract extends AuthController
         //计算出从那条开始查询
         $tol=($page-1)*$limit;
 
-       $list = DB::name('user_extract')->where($where)->field('*')->limit($tol, $limit)->select();
+       $list = DB::name('extract')->where($where)->field('*')->limit($tol, $limit)->select();
 
         $data = array(
             'code'  => 0,
@@ -66,9 +67,41 @@ class Extract extends AuthController
 //var_dump($data);die;
         return $data;
 
+    }
+
+    /**
+     * 详情
+     * @return Request
+     */
+    public function getInfo()
+    {
+        $post = $this->request->post();
+
+        if(!isset($post['id'])){
+            JsonService::fail('缺少参数');
+        }
+        $res =  Db::name('extract')->where('id',$post['id'])->find();
+
+        $this->assign('info',$res);
+
+        return $this->fetch();
+
+    }
 
 
-
+    /**
+     * 提现审核 通过|不通过
+     *
+     * @return json
+     */
+    public function set_status($status='',$id=''){
+        ($status=='' || $id=='') && JsonService::fail('缺少参数');
+        $res=Db::name('extract')->where(['id'=>$id])->update(['status'=>(int)$status]);
+        if($res){
+            return JsonService::successful($status==1 ? '审核通过':'审核不通过');
+        }else{
+            return JsonService::fail($status==-1 ? '审核不通过':'审核通过');
+        }
     }
 
 }
